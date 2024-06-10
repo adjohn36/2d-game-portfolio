8d131bb75296387bbaa3f1168e80c2928cd828f8
adjohn36 <132848782+adjohn36@users.noreply.github.com>
2024-06-09 20:14:52 -0400

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/2d-game-portfolio/",
+    base: "/2d-game-portfolio",
     build: {
         minify: "terser",
     },
